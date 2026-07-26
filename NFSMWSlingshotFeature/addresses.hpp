c59026d55652904a1d1be0de58593062f6a98c99
Kevin4e <kevin.nasufoski@outlook.it>
2026-07-26 20:54:35 +0200

    v1.0.1 update
    v1.0.1 update
    
    Added:
    - N/A
    
    Changed:
    - N/A
    
    Fixed:
    - Fixed a weird bug where, in some situations, the boost would continue to grow/decay during the moment camera.

--- NFSMWSlingshotFeature/addresses.hpp
+++ NFSMWSlingshotFeature/addresses.hpp
@@ -8,24 +8,8 @@ namespace Game {
 	namespace Flags {
 		constexpr std::uintptr_t OpenWorld{ 0x0092D884 };
 		constexpr std::uintptr_t PauseMenu{ 0x0091CAE4 };
+		constexpr std::uintptr_t MomentCamera{ 0x009A3A70 };
 	}
 
 	constexpr std::uintptr_t PerFrameUpdate{ 0x00663EE3 };
-}
-
-namespace Offsets {
-	// Velocity
-	constexpr std::uint8_t VelX{ 0x20 };
-	constexpr std::uint8_t VelY{ 0x24 };
-	constexpr std::uint8_t VelZ{ 0x28 };
-}
-
-namespace Player {
-	constexpr std::uintptr_t Base{ 0x009386C8 };
-
-	namespace Velocity {
-		constexpr std::uintptr_t X{ Base + Offsets::VelX };
-		constexpr std::uintptr_t Y{ Base + Offsets::VelY };
-		constexpr std::uintptr_t Z{ Base + Offsets::VelZ };
-	}
 }
